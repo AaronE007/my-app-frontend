6cb0933a5eb4a0a7c53a1b54a601432fb5969a4e
Aaron Elias <aaronelias007@gmail.com>
2022-07-22 22:36:09 -0400

    Add render to rest list

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -3,11 +3,10 @@ import RestaurantCard from './RestaurantCard'
 
 const RestaurantList = ({restaurants}) => {
 
-  const renderRestaurants = restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant})
+  const renderRestaurants = restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant}/>)
   return (
     <div>
-
-      
+      {renderRestaurants}
     </div>
   )
 }
