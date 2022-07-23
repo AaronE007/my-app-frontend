3d2d0245d26d8031642304ee41531dc260c65e30
Aaron Elias <aaronelias007@gmail.com>
2022-07-22 22:29:07 -0400

    Add Recipe list

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import RecipeCard from './RecipeCard'
 
-const RecipeList = () => {
+const RecipeList = ({meals}) => {
+
+  const renderMeals = meals.map(meals => <RecipeCard key={meals.id} meal={meal}/>)
   return (
     <div>
-      
+      {renderMeals}
     </div>
   )
 }
--- src/containers/RecipeContainer.js
+++ src/containers/RecipeContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import React, {useState, useEffect} from "react";
+import RecipeList from '../components/RecipeList';
 
 
 const RecipeContainer = () => {
