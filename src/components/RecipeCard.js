3f299591aa91f7dd23a12a1dc3734050e5a4badf
Aaron Elias <aaronelias007@gmail.com>
2022-07-22 20:33:08 -0400

    Add data values

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,7 +4,9 @@ const RecipeCard = ({recipe}) => {
   return (
     <div>
       <h3>Name: {recipe.name}</h3>
-      
+      <h4>Calories: {recipe.calories}</h4>
+      <h4>Main Ingredient: {recipe.main_ing}</h4>
+      <h5>Country of Origin: {recipe.country}</h5>
     </div>
   )
 }
