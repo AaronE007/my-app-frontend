2d7004c0ed8e260128a503e94f3104fe83bbbf3a
Aaron Elias <aaronelias007@gmail.com>
2022-07-22 21:11:05 -0400

    Add label to restaurant

--- src/components/RestaurantForm.js
+++ src/components/RestaurantForm.js
@@ -3,7 +3,11 @@ import React from 'react'
 const RestaurantForm = () => {
   return (
     <div>
-      
+      <h2>Add Recipe Here</h2>
+      <form>
+          <label>Name</label>
+          <input/>
+      </form>
     </div>
   )
 }
