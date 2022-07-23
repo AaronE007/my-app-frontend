c1858490b3f0d88708c3c9fc224dff870a8b0e74
Aaron Elias <aaronelias007@gmail.com>
2022-07-22 21:38:33 -0400

    Add useEffect

--- src/containers/RecipeContainer.js
+++ src/containers/RecipeContainer.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const RecipeContainer = () => {
+  const [meals, setmeals] = useState([])
+
+  useEffect(() => {
+    fetch("Url")
+      .then((r) => r.json())
+      .then(setRecipes);
+  }, []);
+  
+  
+  
   return (
     <div>
+
       
     </div>
   )
